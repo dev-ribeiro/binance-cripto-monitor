008cbf32ad57f9e07fb94b353b3232ccacb5b7e8
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-10-15 20:07:24 -0300

    style: create initial interface of all components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,22 @@
-import { Col, Container, Row } from 'react-bootstrap'
+import { Card, Col, Container, Row } from 'react-bootstrap'
 import { DescriptionContainer } from './components/DescriptionContainer'
+import { ResultCriptoQuery } from './components/ResultCriptoQuery'
 import { SearchCripto } from './components/SearchCripto'
 
 export function Home(){
     return (
         <main>
-            <Container className='mt-3'>
+            <Container className='mt-3' style={{width:'45rem'}}>
                 <DescriptionContainer/>
                 <Container className='mt-3 p-2 bg-dark rounded'>
                     <Row>
+                        <Card.Title className='my-2 mx-1 text-light text-left'>Faça sua busca nesse painel:</Card.Title>
                         <Col>
                             <SearchCripto/>
                         </Col>
-                        <Col></Col>
+                        <Col>
+                            <ResultCriptoQuery/>
+                        </Col>
                     </Row>
                 </Container>
             </Container>
