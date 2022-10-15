c737d9bbc1f84cc4ebf6980bd2d7bc5d1dd51e38
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-10-15 17:25:43 -0300

    style: create style for Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,13 @@
-import { Container } from "react-bootstrap";
+import { Container } from 'react-bootstrap'
+import { DescriptionContainer } from './components/DescriptionContainer'
+import styles from './styles.module.css'
 
 export function Home(){
     return (
-        <Container>
-            Hello
-        </Container>
+        <main>
+            <Container className='mt-3'>
+                <DescriptionContainer/>
+            </Container>
+        </main>
     )
 }
