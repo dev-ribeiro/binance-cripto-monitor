0fdd73eae7c3b9861857a4909cd3dc4ca78c8207
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-10-15 17:07:04 -0300

    style: update Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { Figure } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -7,21 +8,25 @@ import styles from './styles.module.css'
 
 export function Header() {
     return (
-        <Navbar expand="lg">
-            <Container className='bg-dark rounded text-light p-2'>
-                <Container className={styles.logo}>
-                    <img src={logo} alt="" />
-                    <h1>BINANCE BOT</h1>
-                </Container>
-                <div className={styles.wrapper}>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link className='text-light' href="/">Home</Nav.Link>
-                            <Nav.Link className='text-light' href="/" target='_blank'>Docs</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </div>
+        <Navbar bg='dark' expand="lg">
+            <Container className='text-light'>
+                <Navbar.Brand className='text-light' href="#home">
+                    <Figure.Image
+                        src={logo}
+                        alt=''
+                        width={50}
+                        height={50}
+                    />
+                    BINANCE BOT
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link className='text-light' href="#home">Home</Nav.Link>
+                        <Nav.Link className='text-light' href="#home">Criptos</Nav.Link>
+                        <Nav.Link className='text-light' href="#link">Docs</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
