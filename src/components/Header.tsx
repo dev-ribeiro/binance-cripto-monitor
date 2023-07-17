81919570adb9ccc66f1481df1d685bd1fc099ff6
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-07-16 23:25:43 -0300

    config: migrate to pnpm

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,7 @@ import { Figure } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from '../../assets/images/logo.png';
-import styles from './styles.module.css';
+import logo from './../assets/images/logo.png';
 
 export function Header() {
   return (
